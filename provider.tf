911b4202d3c3025dbde2b552e78384d172fdbe09
Haniben <Haniben.bhavsar@einfochips.com>
2024-03-07 17:19:41 +0530

    changes in some file

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "sa1_test_eic_HaniBhavsar"
+    storage_account_name = "storeststefile"
+    container_name       = "statefile"
+    key                  = "terraform.tfstate"
+  }
+}
 }
 
 provider "azurerm" {
